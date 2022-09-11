9d6b67319a7794b1e866728e9505bde7d9e5ac69
edwinfernaldy <edwinfernaldy@gmail.com>
2022-09-11 15:52:40 +0700

    reconfig prettier

---
+++ .prettier-plugin.js
@@ -0,0 +1,21 @@
+const tailwind = require('prettier-plugin-tailwindcss')
+const organizeImports = require('prettier-plugin-organize-imports')
+// const solidity = require('prettier-plugin-solidity')
+
+const combinedFormatter = {
+  ...tailwind,
+  parsers: {
+    ...tailwind.parsers,
+    ...Object.keys(organizeImports.parsers).reduce((acc, key) => {
+      acc[key] = {
+        ...tailwind.parsers[key],
+        preprocess(code, options) {
+          return organizeImports.parsers[key].preprocess(code, options)
+        },
+      }
+      return acc
+    }, {}),
+  },
+}
+
+module.exports = combinedFormatter
