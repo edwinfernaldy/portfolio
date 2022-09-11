f854a3af9773c3dc37e23463b0fe38ec59f054cd
edwinfernaldy <edwinfernaldy@gmail.com>
2022-09-11 14:03:42 +0700

    add lorem ipsum to landing section

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -3,17 +3,13 @@ import styles from "../styles/Home.module.css";
 
 export default function LandingSection() {
   return (
-    <section
-      className={cn(
-        "h-screen w-full p-6 flex items-center justify-center",
-        styles.bgGradient
-      )}
-    >
-      <h1 className="xl:text-9xl text-2xl text-cyan-800 font-extrabold">
-        WEBSITE DEVELOPER
+    <section className={cn("h-screen p-6")}>
+      <h1 className="xl:text-3xl text-2xl text-white">
+        Some Content Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Pariatur excepturi non minima, cupiditate ipsum numquam nulla fuga
+        corrupti asperiores nostrum quis quas illo doloribus, quisquam
+        blanditiis ratione amet repellat sed.
       </h1>
-
-      <p>Edwin Fernaldy</p>
     </section>
   );
 }
