e3361c81ce84bcb0f98759e1c6cb80f23efc98be
edwinfernaldy <edwinfernaldy@gmail.com>
2022-09-15 14:03:46 +0700

    add color palette

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -1,15 +1,17 @@
-import cn from "clsx";
-import styles from "../styles/Home.module.css";
+import { Parallax } from "react-scroll-parallax";
 
 export default function LandingSection() {
   return (
-    <section className={cn("h-screen p-6")}>
-      <h1 className="xl:text-3xl text-2xl text-white">
-        Some Content Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Pariatur excepturi non minima, cupiditate ipsum numquam nulla fuga
-        corrupti asperiores nostrum quis quas illo doloribus, quisquam
-        blanditiis ratione amet repellat sed.
-      </h1>
+    <section className='flex h-screen items-center lg:px-28 px-14'>
+      <Parallax easing={"easeInOutCirc"} speed={50}>
+        <h1 className='lg:text-7xl text-3xl'>
+          Hi,
+          <br />
+          I&#39;m <span>E</span>dwin <span>F</span>ernaldy,
+          <br />
+          web developer
+        </h1>
+      </Parallax>
     </section>
   );
 }
