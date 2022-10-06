ad16f582cbb6d92de94e9942fe406dc0822429c1
edwinfernaldy <edwinfernaldy@gmail.com>
2022-10-06 12:53:09 +0700

    use Tilt

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -1,32 +1,40 @@
 import Image from "next/image";
 import { Parallax } from "react-scroll-parallax";
 import DivingImage from "/public/assets/photos/IMG_2727.jpeg";
+import Tilt from "react-parallax-tilt";
 
 export default function LandingSection() {
   return (
     <section className='flex h-screen items-center'>
-      <Parallax
-        easing={"easeInOutCirc"}
-        className='flex-grow peer z-20'
-        speed={20}
-      >
-        <h1 className='lg:text-7xl text-3xl'>
-          Hi,
-          <br />
-          I&#39;m <span>E</span>dwin <span>F</span>ernaldy,
-          <br />
-          web developer
-        </h1>
+      <Parallax easing={"easeInOutCirc"} className='peer z-20' speed={20}>
+        <Tilt>
+          <h1 className='lg:text-7xl text-3xl'>
+            Hi,
+            <br />
+            I&#39;m <span className='text-blue-400'>E</span>dwin{" "}
+            <span className='text-pink-300'>F</span>
+            ernaldy,
+            <br />
+            web developer
+          </h1>
+        </Tilt>
       </Parallax>
 
-      <div className='absolute inset-0 duration-300 transition peer-hover:blur-lg'>
+      <Tilt
+        tiltReverse
+        transitionSpeed={800}
+        tiltMaxAngleX={5}
+        tiltMaxAngleY={5}
+        scale={1.2}
+        className='absolute inset-0 duration-300 transition peer-hover:blur-lg'
+      >
         <Image
           className='object-cover brightness-50'
           src={DivingImage}
           layout='fill'
           alt='Background-image'
         />
-      </div>
+      </Tilt>
     </section>
   );
 }
