ac89e0416a98bb7501a72421ced7bb9e92951e81
edwinfernaldy <edwinfernaldy@gmail.com>
2022-10-13 12:41:24 +0700

    new landing section

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -1,40 +1,49 @@
 import Image from "next/image";
 import { Parallax } from "react-scroll-parallax";
 import DivingImage from "/public/assets/photos/IMG_2727.jpeg";
-import Tilt from "react-parallax-tilt";
+import Jumbotron from "./Jumbotron";
+import DevBooks from "/public/assets/photos/webdevbook.jpeg";
 
 export default function LandingSection() {
   return (
-    <section className='flex h-screen items-center'>
-      <Parallax easing={"easeInOutCirc"} className='peer z-20' speed={20}>
-        <Tilt>
-          <h1 className='lg:text-7xl text-3xl'>
-            Hi,
-            <br />
-            I&#39;m <span className='text-blue-400'>E</span>dwin{" "}
-            <span className='text-pink-300'>F</span>
-            ernaldy,
-            <br />
-            web developer
-          </h1>
-        </Tilt>
-      </Parallax>
+    <section className='flex flex-col lg:flex-row p-5 h-screen items-center'>
+      <div className='flex flex-col gap-2 basis-1/2'>
+        <div className='relative h-1/2'>
+          <Image
+            className='object-cover aspect-square saturate-0'
+            src={DevBooks}
+            width={600}
+            height={400}
+            alt='Background-image'
+          />
+        </div>
 
-      <Tilt
-        tiltReverse
-        transitionSpeed={800}
-        tiltMaxAngleX={5}
-        tiltMaxAngleY={5}
-        scale={1.2}
-        className='absolute inset-0 duration-300 transition peer-hover:blur-lg'
-      >
-        <Image
-          className='object-cover brightness-50'
-          src={DivingImage}
-          layout='fill'
-          alt='Background-image'
-        />
-      </Tilt>
+        <div className='h-1/2 relative'>
+          <Image
+            className='object-cover aspect-square saturate-0'
+            src={DivingImage}
+            width={600}
+            height={400}
+            alt='Background-image'
+          />
+        </div>
+      </div>
+
+      <div className=' basis-1/2 flex flex-col lg:min-h-screen justify-between py-5'>
+        <Jumbotron className='self-start xl:text-9xl font-bold pb-2 border-b border-gray-500'>
+          PORTFOLIO
+        </Jumbotron>
+
+        <div>
+          <p>Edwin Fernaldy</p>
+
+          <p>Website Developer</p>
+        </div>
+
+        <div>
+          <p className='font-neue text-center'>Scroll down for more</p>
+        </div>
+      </div>
     </section>
   );
 }
