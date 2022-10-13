0db0e486905dc1ed81399918876710c9e9af1e75
edwinfernaldy <edwinfernaldy@gmail.com>
2022-10-13 12:19:18 +0700

    remove css

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,10 @@ import AboutSection from "../components/AboutSection";
 import LandingSection from "../components/LandingSection";
 import LoadingScreen from "../components/LoadingScreen";
 import Navbar from "../components/Navbar";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Edwin Fernaldy | Web Developer</title>
         <meta
@@ -20,28 +19,13 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <Navbar />
-
         <div className='lg:px-28 px-14'>
           <LandingSection />
 
           <AboutSection />
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    </>
   );
 };
 
