6a104c48b8149c666a9c99ee749d807d995419dd
edwinfernaldy <edwinfernaldy@gmail.com>
2022-10-14 19:50:57 +0700

    use lorem ipsum temp

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Jumbotron from "./Jumbotron";
 import SingAlley from "/public/assets/photos/IMG_1459.jpg";
+import Guitaring from "/public/assets/photos/IMG_3204.jpeg";
 
 export default function aboutSection() {
   return (
@@ -8,14 +9,14 @@ export default function aboutSection() {
       <div className='relative w-full flex-grow h-full'>
         <Image
           className='saturate-0 h-full'
-          src={SingAlley}
+          src={Guitaring}
           layout='fill'
           objectFit='cover'
           alt='close-up-me'
         />
       </div>
 
-      <div className='p-6 flex flex-col gap-4'>
+      <div className='px-6 flex flex-col gap-6'>
         <Jumbotron className='mb-5 xl:text-7xl'>About Me</Jumbotron>
 
         <div className='flex flex-col md:flex-row gap-5'>
@@ -39,14 +40,57 @@ export default function aboutSection() {
           </div>
         </div>
 
-        <div className='relative h-full'>
-          <Image
-            layout='fill'
-            objectFit='cover'
-            className='h-full saturate-0'
-            src={SingAlley}
-            alt='me'
-          />
+        <div>
+          <h1 className='text-3xl'>Thanks for the attention</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic aliquid
+            sed necessitatibus, dignissimos corrupti quaerat obcaecati. Ducimus
+            unde doloribus deleniti, numquam dolorem, doloremque, explicabo
+            eveniet quod perspiciatis voluptate quisquam nulla.
+          </p>
+        </div>
+
+        <div className='flex items-start gap-2 h-full'>
+          <div className='relative w-full h-full'>
+            <Image
+              layout='fill'
+              objectFit='cover'
+              className='h-full saturate-0'
+              src={SingAlley}
+              alt='me'
+            />
+          </div>
+
+          <div className='flex flex-col justify-between h-full'>
+            <h1>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              perferendis neque nihil corporis earum, libero qui et odit atque
+              ipsa praesentium perspiciatis aut vero dicta aspernatur sunt!
+              Iusto, hic nihil!
+            </h1>
+
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis
+              minima assumenda at, quis perferendis veritatis odit quam corrupti
+              autem! Odit ipsa vitae vero facere sed cupiditate temporibus sint
+              ex repudiandae.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis
+              minima assumenda at, quis perferendis veritatis odit quam corrupti
+              autem! Odit ipsa vitae vero facere sed cupiditate temporibus sint
+              ex repudiandae.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis
+              minima assumenda at, quis perferendis veritatis odit quam corrupti
+              autem! Odit ipsa vitae vero facere sed cupiditate temporibus sint
+              ex repudiandae.
+            </p>
+          </div>
         </div>
       </div>
     </section>
