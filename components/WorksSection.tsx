cc2234be23d579c2dfb6ad6be1a662a88a9b3ec6
edwinfernaldy <edwinfernaldy@gmail.com>
2022-10-20 21:05:48 +0700

    edit title

--- components/WorksSection.tsx
+++ components/WorksSection.tsx
@@ -1,11 +1,15 @@
 export default function WorksSection() {
   return (
     <section className='py-6'>
-      <div className='relative'>
-        <h1 className='text-5xl w-fit px-6'>My Previous Works</h1>
+      <div className='relative h-12'>
+        <div className='absolute w-fit mx-6 bg-[#edeced] z-10'>
+          <h1 className='text-5xl'>My Previous Works</h1>
+        </div>
 
-        <div className='absolute top-5 inset-x-0 border border-black w-full'></div>
+        <span className='absolute top-5 inset-x-0 border border-black w-full'></span>
       </div>
+
+      <div></div>
     </section>
   );
 }
