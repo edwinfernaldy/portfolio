b4cb2d135e87d24e2d9442e206c6aa6045492d36
edwinfernaldy <edwinfernaldy@gmail.com>
2022-11-16 16:36:44 +0700

    add parallax effects at landing

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 import DivingImage from "/public/assets/photos/IMG_2727.jpeg";
 import Jumbotron from "./Jumbotron";
 import DevBooks from "/public/assets/photos/webdevbook.jpeg";
+import { useParallax } from "react-scroll-parallax";
 
 export default function LandingSection() {
+  const op = useParallax<HTMLDivElement>({ opacity: [10, 0] });
+
+  const scale = useParallax<HTMLDivElement>({ translateY: [80, -10] });
+
   return (
     <section className='flex flex-col lg:flex-row p-5 gap-2 h-screen overflow-hidden items-center'>
-      <div className='flex flex-col gap-2 flex-grow w-full h-full'>
+      <div ref={op.ref} className='flex flex-col gap-2 flex-grow w-full h-full'>
         <div className='relative h-1/2 overflow-hidden'>
           <Image
             className='object-cover saturate-0 h-full'
@@ -26,7 +31,7 @@ export default function LandingSection() {
         </div>
       </div>
 
-      <div className='flex flex-col h-full justify-center'>
+      <div ref={scale.ref} className='flex flex-col h-full justify-center'>
         <Jumbotron className='self-start xl:text-9xl font-bold pb-2 border-b border-gray-500'>
           PORTFOLIO
         </Jumbotron>
