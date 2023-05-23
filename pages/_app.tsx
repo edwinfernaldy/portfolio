5e76e3a697d2aec8d54c5967e4f118d7ef3bec16
edwinfernaldy <edwinfernaldy@gmail.com>
2023-05-23 20:02:27 +0700

    add analytics to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ParallaxProvider } from "react-scroll-parallax";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ParallaxProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ParallaxProvider>
   );
 }
