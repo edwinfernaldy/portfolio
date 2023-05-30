147b330223598938aa97ad0b652ea96e19670415
edwinfernaldy <edwinfernaldy@gmail.com>
2023-05-30 13:14:17 +0700

    add dialog

--- components/ExpSub.tsx
+++ components/ExpSub.tsx
@@ -8,7 +8,7 @@ interface ExpSubType {
 
 export default function ExpSub(props: ExpSubType) {
   return (
-    <div className={cn("w-1/4 self-center h-fit", props.className)}>
+    <div className={cn("relative self-center h-fit", props.className)}>
       <p>{props.children}</p>
     </div>
   );
